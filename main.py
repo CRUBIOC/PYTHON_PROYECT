e072f4cf7107998b5cdaa029dd8a074bb8cad73a
[carolina rubio] <[carolinarubio-12@hotmail.com]>
2023-09-23 19:23:16 -0500

    conflicto y que?

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ if __name__ == "__main__":
     print("holi mochos")
     print("llegue de primis bye")
     print("llegue seguns mores")
+    print("conflicto HIJUEPUTA")
     print("holi att: Cortes")
