b39495c909ce1385204bb6516f3aba0f7eafb403
Juan David Londoño <jlondonocorrea@gmail.com>
2023-09-30 17:51:00 -0500

    creamos pagina html en el localhost

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
+    name="static",
+)
+
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def read_root(request: Request):
-    data = {"message": "Pagina de inicio del proyecto pagina web de peliculas."}
+    data = {"message": "Página de inicio del proyecto página web de películas."}
 
-    return templates.TemplateResponse("main_template.html", {"request": request, "data": data})
+    return templates.TemplateResponse(
+        "main_template.html", {"request": request, "data": data}
+    )
+
+
+@app.get("/JuanD")
+def Test_JuanD(request: Request):
+    data = {"message": "Se hace lo que se puede"}
+
+    return templates.TemplateResponse("Prueba.html", {"request": request, "data": data})
