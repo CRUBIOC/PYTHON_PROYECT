ccd1ccdd2023752134fe716b681c4632af20e9bf
SebastianC <sebastiancortes406@gmail.com>
2023-09-30 17:51:52 -0500

    Se creo HTML en localHost y archivos CSS

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from fastapi.staticfiles import StaticFiles 
+from pathlib import Path
 
 app = FastAPI()
+
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.absolute() / "static"),
+    name="static",
+)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -12,3 +20,15 @@ def read_root(request: Request):
 
     return templates.TemplateResponse("main_template.html", {"request": request, "data": data})
 
+@app.get("/cortes")
+def read_root(request: Request, nombre:str=None):
+    if nombre == None:
+        data = {"message": f"No recibi informacion de la variable"}
+    else:        
+        data = {"message": f"{nombre}"}
+
+    return templates.TemplateResponse("main_cortes.html", {"request": request, "data": data})
+
+@app.get("/multiplicar2")
+def multiplicar2(number:int):
+    return number*2
